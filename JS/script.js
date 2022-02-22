eba00e4e6b73a92a343351f278769668c00d5886
Vladimir <vovan0503@mail.ru>
2022-02-22 18:16:38 +0300

    Add loading cards

--- JS/script.js
+++ JS/script.js
@@ -80,12 +80,104 @@ window.addEventListener('load', () => {
         } else {
             count = 2;
         };
-        changeSlide(count);  
-    }; 
+        changeSlide(count);
+    };
 
     arrowRight.addEventListener('click', increment);
     arrowLeft.addEventListener('click', decrement);
 
 
-    //
+    //Repair carts
+
+
+    class Card {
+        constructor(parent, html) {
+            this.parent = document.querySelector(parent);
+            this.html = html;
+        };
+
+        render() {
+            this.parent.insertAdjacentHTML('beforeend', this.html);
+        }
+    }
+
+    class TypeCard extends Card {
+        constructor(imgSrc, title, price, parent, html) {
+            super(parent, html)
+            this.imgSrc = imgSrc;
+            this.title = title;
+            this.price = price;
+        }
+    }
+
+    fetch('/db.json')
+        .then(res => res.json())
+        .then(({ types }) => {
+            types.forEach(({ imgSrc, title, price }) => new TypeCard(imgSrc, title, price, '.repair_block',
+                `
+                    <div class="repair_block__option">
+                        <img class="repair_image" src=${imgSrc}>
+                        <div class="repair_block__option__text">
+                            <p>${title}</p>
+                            <ul>
+                                <li>Dismantling of old coatings</li>
+                                <li>Wallpapering</li>
+                                <li>Painting walls and ceilings</li>
+                                <li>Laying flooring</li>
+                                <li>Replacing plumbing</li>
+                                <li>Replacing sockets and switches</li>
+                                <li>Replacing doors</li>
+                            </ul>
+                            <div class="repair_block__price">
+                                <h3 class="heading heading_price">from ${price} rub / m2</h3>
+                            </div>
+                        </div>
+                    </div>
+                `
+            ).render())
+        });
+
+
+    // Client carts
+    class ClientCard extends Card {
+        constructor(name, avatar, meters, days, parent, html) {
+            super(parent, html);
+            this.name = name;
+            this.avatar = avatar;
+            this.meters = meters;
+            this.days = days;
+        }
+    };
+
+    fetch('/db.json')
+        .then(res => res.json())
+        .then(({ clients }) => {
+            clients.forEach(({ name, avatar, meters, days, }) => new ClientCard(name, avatar, meters, days, '.section_3__block_2__clients_block',
+                `
+                    <div class="client">
+                        <div class="client__content">
+                            <div class="client_logo">
+                                <img src=${avatar}>
+                                <h3 class="heading heading_7">${name}</h3>
+                            </div>
+                            <p class="paragraph client_review">I want to thank the guys for their work. We put your<br>
+                                shoer part in repair. I am very grateful that I referred<br>
+                                to the repair as to my own. Great thanks i express the<br>
+                                project to georgia for its professionalism and approach.</p>
+                            <p class="paragraph client_review">The repair process was turned out in terms, faster
+                                than<br>
+                                anticipated. Yeam mobile and professionalism. Works<br>
+                                performed qualitatively without comments. Quality<br>
+                                satisfied. Prices democratic. I recommend these<br> masters.</p>
+                        </div>
+                        <button class="button client_block__button" name="button"><img class="button_vector"
+                                src="./images/button_vect1.svg">
+                            <span class="button__text client_block__button_text">${meters} m2</span>
+                            <div class="delimiter"></div><img class="button_vector" src="./images/button_vect2.svg">
+                            <span class="button__text client_block__button_text">${days} DAYS</span>
+                        </button>
+                    </div>
+                `
+            ).render())
+        });
 });
